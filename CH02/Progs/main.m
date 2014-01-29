94d2dd43550b330ff1a67aa435f0412ef8299fd0
Yuwei Wang <ywwang@mac>
2014-01-30 01:58:05 +0800

    20140130-0146
    CH02

---
+++ CH02/Progs/main.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+	@autoreleasepool {
+		NSLog(@"Programming is fun!!!!");
+	}
+	return 0;
+}
---
+++ CH02/prog1/prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by wyw on 1/30/14.
+//  Copyright (c) 2014 wyw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Programming is fun!");
+        
+    }
+    return 0;
+}
+
