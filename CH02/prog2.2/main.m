cfde3f8861741d1f2a58b46c51f0c791fcf95fdb
Yuwei Wang <ywwang@mac>
2014-02-01 02:07:53 +0800

    20140201-0207
    CH02

---
+++ CH02/prog2.2/main.m
@@ -0,0 +1,10 @@
+#include <Foundation/Foundation.h>
+
+int main()
+{
+	@autoreleasepool {
+		NSLog(@"Programming is fun!");
+		NSLog(@"Programming in Objective-C is even more fun!");
+	}
+	return 0;
+}
