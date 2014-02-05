87dc5426d316086e653f5c5273914a0208be7dcf
Yu-Wei Wang <wyw@GT628>
2014-02-05 15:33:38 +0800

    20140205-1533
    CH03

---
+++ CH03/prog3.1/main.m
@@ -0,0 +1,12 @@
+// Simple program to work with fractions
+#import <Foundation/Foundation.h>
+
+int main (int argc, char *argv[])
+{
+	@autoreleasepool {
+		int numerator = 1;
+		int denominator = 3;
+		NSLog(@"The fraction is %i/%i", numerator, denominator);
+	}
+	return 0;
+}
---
+++ CH03/prog3.2/main.m
@@ -0,0 +1,57 @@
+// Program to work with fractions iV class version
+
+#import <Foundation/Foundation.h>
+
+// ---- @interface section ----
+@interface Fraction: NSObject
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+
+@end
+
+// ---- @implementation section ----
+@implementation Fraction
+{
+	int numerator;
+	int denominator;
+}
+
+-(void) print
+{
+	NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator: (int) n
+{
+	numerator = n;
+}
+
+-(void) setDenominator: (int) d
+{
+	denominator = d;
+}
+
+@end
+
+// ---- program section ----
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+		Fraction *myFraction;
+
+		// create an instance of a Fraction
+		myFraction = [Fraction alloc];
+		myFraction = [myFraction init];
+
+		// Set fraction to 1/3
+		[myFraction setNumerator: 1];
+		[myFraction setDenominator: 3];
+
+		// Display the fraction using the print method
+		NSLog(@"The value of myFraction is:");
+		[myFraction print];
+	}
+	return 0;
+}
