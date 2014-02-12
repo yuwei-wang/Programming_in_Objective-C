58ce7fc5bf1a80d9ec74b6a85932b6affc62e6a0
Yu-Wei Wang <wyw@GT628>
2014-02-12 18:42:27 +0800

    20140212-1842
    CH05

---
+++ CH05/prog5.2/main.m
@@ -0,0 +1,10 @@
+// Program to calculate the 200th triangular number
+// Indroduction of the for statement
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
