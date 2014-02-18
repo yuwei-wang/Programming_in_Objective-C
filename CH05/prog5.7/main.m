abc7c6b940bd5bffaf6497d40b4b69ebbd520adb
Yu-Wei Wang <wyw@GT628>
2014-02-18 17:18:38 +0800

    20140218-1718
    CH05

---
+++ CH05/prog5.7/main.m
@@ -0,0 +1,22 @@
+// find the greastest common divisor of two nonnegative integers
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+		unsigned int u, v, temp;
+
+		NSLog(@"Please type in two nonnegative integers.");
+		scanf("%u%u", &u, &v);
+
+		while(v != 0) 
+		{
+			temp = u % v;
+			u = v;
+			v = temp;
+		}
+		NSLog(@"Their greastest common divisor is %u", u);
+	}
+	return 0;
+}
