d27a11fc7900fecb3364909f129229f36247b715
Yuwei Wang <ywwang@mac>
2014-03-04 21:43:33 +0800

    20140304-2143
    CH07

---
+++ CH07/prog7.4b/main.m
@@ -0,0 +1,24 @@
+#import "Fraction.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+		Fraction *aFraction = [[Fraction alloc] init];
+		Fraction *bFraction = [[Fraction alloc] init];
+
+		Fraction *resultFraction;
+
+		[aFraction setTo: 1 over: 4]; // set 1sr Fraction to 1/4
+		[bFraction setTo: 1 over: 2]; // set 2nd Fraction to 1/2
+
+		[aFraction print];
+		NSLog(@"+");
+		[bFraction print];
+		NSLog(@"=");
+		
+		resultFraction = [aFraction add: bFraction];
+		[resultFraction print];
+	}
+
+	return 0;
+}
