edc8d49510129a6238c6a05898e4a90f7acb4ad9
Yu-Wei Wang <wyw@GT628>
2014-03-11 18:57:15 +0800

    20140311-1857
    CH07

---
+++ CH07/ex7.1/Fraction.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+// Define the Fraction class
+
+@interface Fraction : NSObject
+@property int numerator, denominator;
+-(void) print;
+-(void) setTo: (int) n over: (int) d;
+-(double) convertToNum;
+//-(void) add: (Fraction *) f;
+-(Fraction *) add: (Fraction *) f;
+-(Fraction *) subtract: (Fraction *) f;
+//-(Fraction *) multiply: (Fraction *) f;
+//-(Fraction *) divide: (Fractieon *) f;
+-(void) reduce;
+@end
