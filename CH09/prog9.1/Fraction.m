e3a02fb1083b02271090787cc04be552cf013461
Yu-Wei Wang <wyw@GT628>
2014-03-12 18:35:44 +0800

    20140312-1835
    CH09

---
+++ CH09/prog9.1/Fraction.m
@@ -0,0 +1,59 @@
+#import "Fraction.h"
+
+@implementation Fraction
+@synthesize numerator, denominator;
+-(void) print
+{
+	NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(double) convertToNum
+{
+	if (denominator != 0)
+	{
+		return (double) numerator / denominator;
+	} else {
+		return NAN;
+	}
+}
+
+-(void) setTo: (int) n over: (int) d
+{
+	numerator = n;
+	denominator = d;
+}
+
+// add a Fraction to the receiver
+-(Fraction *) add: (Fraction *) f
+{
+	// To add two Fractions:
+	// a/b + c/d = ((a * d) + (C * b)) / b * d
+
+	// result will store the result of the addition
+	Fraction *result = [[Fraction alloc] init];
+
+	result.numerator = (numerator * f.denominator) + (f.numerator * denominator);
+	result.denominator = denominator * f.denominator;
+
+	[result reduce];
+
+	return result;
+}
+
+-(void) reduce
+{
+	int u = numerator;
+	int v = denominator;
+	int temp;
+
+	while(v != 0)
+	{
+		temp = u % v;
+		u = v;
+		v = temp;	
+	}
+	
+	numerator /= u;
+	denominator /= u;
+}
+@end
